2f2c6e09a76fd1b68cc5e18d7329111b9dda25de
cornchiplick <ilsunkiss@gmail.com>
2022-09-21 17:19:46 +0900

    20220921

---
+++ Algorithm/src/main/java/Sort_Counting.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Sort_Counting {
+
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(in.readLine());
+    int[] cnt = new int[10000002];
+
+    for (int i = 0; i < N; i++) {
+    cnt[Integer.parseInt(in.readLine())]++;
+    }
+
+    in.close();
+
+    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    for (int i = 0; i <= N; i++) {
+      while (cnt[i] > 0) {
+        out.write(String.valueOf(i));
+        out.write('\n');
+        cnt[i]--;
+      }
+    }
+    out.flush();
+
+    out.close();
+  }
+
+
+}
