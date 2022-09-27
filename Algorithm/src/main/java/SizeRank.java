40e5dd459c62e85022a52426b36edbce823a97ed
cornchiplick <ilsunkiss@gmail.com>
2022-09-27 09:04:50 +0900

    20220927

--- Algorithm/src/main/java/SizeRank.java
+++ Algorithm/src/main/java/SizeRank.java
@@ -1,22 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class SizeRank {
 
   public static void main(String[] args) throws Exception {
 
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuilder out = new StringBuilder();
 
-    int N = Integer.parseInt(in.readLine());
+    int N = Integer.parseInt(br.readLine());
     int[][] bmi = new int[N][2];
 
     for (int i = 0; i < N; i++) {
-      bmi[i][0] = Integer.parseInt(in.readLine());
-      bmi[i][1] = Integer.parseInt(in.readLine());
+      StringTokenizer in = new StringTokenizer(br.readLine());
+      bmi[i][0] = Integer.parseInt(in.nextToken());
+      bmi[i][1] = Integer.parseInt(in.nextToken());
     }
 
-    
-
+    int count = 1;
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (bmi[i][0] < bmi[j][0] && bmi[i][1] < bmi[j][1]) {
+          count++;
+        }
+      }
+      out.append(count).append(" ");
+      count = 1;
+    }
+    System.out.println(out);
   }
 }
