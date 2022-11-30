91eaba21aaabfeb0955f19c9b26a589349165393
cornchiplick <ilsunkiss@gmail.com>
2022-11-30 21:06:33 +0900

    20221130
    직사각형 넓이 구하기, 다항식 더하기

---
+++ Algorithm/src/main/java/programmerslv0/SafeZone.java
@@ -0,0 +1,24 @@
+package programmerslv0;
+
+public class SafeZone {
+
+  public static void main(String[] args) {
+
+    int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
+    int result = 16;
+    System.out.println(solution(board));
+
+  }
+
+  public static int solution(int[][] board) {
+    int n = board.length;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+         
+      }
+    }
+
+    return 0;
+  }
+}
