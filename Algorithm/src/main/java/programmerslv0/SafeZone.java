9884228c24870e62f1e09caf75364c3e9835148c
cornchiplick <ilsunkiss@gmail.com>
2022-12-01 17:58:03 +0900

    20221201
    안전지대, 저주의 숫자 3

--- Algorithm/src/main/java/programmerslv0/SafeZone.java
+++ Algorithm/src/main/java/programmerslv0/SafeZone.java
@@ -13,12 +13,64 @@ public class SafeZone {
   public static int solution(int[][] board) {
     int n = board.length;
 
+    int starti;
+    int endi;
+    int startj;
+    int endj;
+
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < n; j++) {
-         
+        if (board[i][j] != 1) {
+          // i 검사 시작 위치 설정
+          if (i == 0) {
+            starti = 0;
+          } else {
+            starti = i - 1;
+          }
+
+          // i 검사 끝 위치 설정
+          if (i == n - 1) {
+            endi = n - 1;
+          } else {
+            endi = i + 1;
+          }
+
+          // j 검사 시작 위치 설정
+          if (j == 0) {
+            startj = 0;
+          } else {
+            startj = j - 1;
+          }
+
+          // j 검사 끝 위치 설정
+          if (j == n - 1) {
+            endj = n - 1;
+          } else {
+            endj = j + 1;
+          }
+
+          loop:
+          for (int inspecti = starti; inspecti <= endi; inspecti++) {
+            for (int inspectj = startj; inspectj <= endj; inspectj++) {
+              if (board[inspecti][inspectj] == 1) {
+                board[i][j] = 2;
+                break loop;
+              }
+            }
+          }
+        }
       }
     }
 
-    return 0;
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (board[i][j] != 0) {
+          count++;
+        }
+      }
+    }
+
+    return n*n - count;
   }
 }
