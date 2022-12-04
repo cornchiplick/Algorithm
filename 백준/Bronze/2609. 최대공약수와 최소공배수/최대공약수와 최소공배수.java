8b41df7c141258c33a15dd100c918ccae55fe100
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2022-12-04 16:49:25 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 208 ms, Memory: 17776 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,43 +1,21 @@
-import java.lang.Math;
 import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
 
-    int n = in.nextInt();
-    int m = in.nextInt();
-    int[] max_n = new int[n];
-    int[] max_m = new int[m];
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
     int max = -1;
-
-    for (int i = 0; i <= n - 1; i++) {
-      if (n % (i + 1) == 0) {
-        max_n[i] = i + 1;
-      }
-    }
-
-    for (int i = 0; i <= m - 1; i++) {
-      if (m % (i + 1) == 0) {
-        max_m[i] = i + 1;
-      }
-    }
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if ((max_n[i] == max_m[j]) && (max < max_n[i])) {
-          max = max_n[i];
-        }
+    for (int i = 1; i <= Math.min(a, b); i++) {
+      if (max < i && a % i == 0 && b % i == 0) {
+        max = i;
       }
     }
     System.out.println(max);
+    System.out.println(a * b / max);
 
-    for (int i = 1; i <= n * m; i++) {
-      if ((i % n == 0) && (i % m == 0)) {
-        System.out.println(i);
-        break;
-      }
-    }
   }
 }
