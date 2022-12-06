bb778062dc29835d1ebe2dc3df1342d7b8e9f833
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2022-12-06 17:37:42 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.70 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,63 @@
+import java.util.*;
+class Solution {
+public int solution(int bridge_length, int weight, int[] truck_weights) {
+    Queue<Truck> line = new LinkedList<>();
+    Queue<Truck> over = new LinkedList<>();
+
+    for (int element : truck_weights) {
+      line.offer(new Truck(element, 0));
+    }
+
+    // 시간 경과
+    int count = 1;
+
+    // over Queue에 첫번째 원소 세팅
+    // 다리 위에 가해지는 하중
+    int wei = line.peek().gravity;
+    line.peek().location++;
+    over.offer(line.poll());
+
+    // over Queue가 빌 때까지 진행
+    while (!over.isEmpty()) {
+      count++;
+
+      // 다리 위에 있는 모든 트럭들 1칸 전진
+      for (Truck element : over) {
+        element.location++;
+      }
+
+      // 다리 위, 맨 앞 트럭이 다리를 지났는지 확인하기
+      if (over.peek().location > bridge_length) {
+        // 지났으면 다리에 가해지는 하중에서 트럭무게만큼 빼기
+        wei -= over.peek().gravity;
+        // 다리에서 트럭 제거
+        over.poll();
+      }
+
+      // 대기열이 비어있는지 확인
+      if (!line.isEmpty()) {
+        // 비었으면 다리위에 가해지는 하중에 더 추가해도 되는지 확인
+        if (wei + line.peek().gravity <= weight) {
+          // 괜찮으면 다리 위에 올리기
+          line.peek().location++;
+          wei += line.peek().gravity;
+          over.offer(line.poll());
+        }
+      }
+
+
+    }
+
+    return count;
+  }
+}
+
+class Truck {
+  int gravity;
+  int location;
+
+  Truck(int gravity, int location) {
+    this.gravity = gravity;
+    this.location = location;
+  }
+}
