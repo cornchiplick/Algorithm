b17d704f249e85ef91d9fd38b510ce8cd3349bca
cornchiplick <ilsunkiss@gmail.com>
2022-12-14 21:36:07 +0900

    Update
    baekjoon: 나이순 정렬. 보기좋게 정리

--- Algorithm/src/main/java/programmerslv0/AgeSort.java
+++ Algorithm/src/main/java/programmerslv0/AgeSort.java
@@ -3,7 +3,6 @@ package programmerslv0;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class AgeSort {
@@ -18,24 +17,31 @@ public class AgeSort {
 
     for (int i = 0; i < N; i++) {
       StringTokenizer st = new StringTokenizer(in.readLine());
-      humans[i] = new Person();
-      humans[i].age = Integer.parseInt(st.nextToken());
-      humans[i].name = st.nextToken();
-      humans[i].no = i;
+      humans[i] = new Person(Integer.parseInt(st.nextToken()), st.nextToken(), i);
     }
 
     in.close();
 
-    Arrays.sort(humans, new Comparator<Person>() {
-      @Override
-      public int compare(Person o1, Person o2) {
-        if (o1.age == o2.age) {
-          return o1.no - o2.no;
-        } else {
-          return o1.age - o2.age;
-        }
+    Arrays.sort(humans, ((o1, o2) -> {
+      if (o1.age == o2.age) {
+        return o1.no - o2.no;
+      } else {
+        return o1.age - o2.age;
       }
-    });
+    }));
+
+    // ====================================================
+
+//    Arrays.sort(humans, new Comparator<Person>() {
+//      @Override
+//      public int compare(Person o1, Person o2) {
+//        if (o1.age == o2.age) {
+//          return o1.no - o2.no;
+//        } else {
+//          return o1.age - o2.age;
+//        }
+//      }
+//    });
 
     for (Person human : humans) {
       out.append(human.age).append(" ").append(human.name).append('\n');
@@ -49,6 +55,12 @@ public class AgeSort {
     int age;
     String name;
     int no;
+
+    Person(int age, String name, int no) {
+      this.age = age;
+      this.name = name;
+      this.no = no;
+    }
   }
 
 }
