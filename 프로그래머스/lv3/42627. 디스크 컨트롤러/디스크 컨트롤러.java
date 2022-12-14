6809f9266dfe019ac34809ecc25ba4ca202785f6
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2022-12-14 17:10:44 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.47 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+public int solution(int[][] jobs) {
+    int answer = 0;
+
+    Arrays.sort(jobs, (o1, o2) -> {
+      if (o1[0] == o2[0]) {
+        return o1[1] - o2[1];
+      } else {
+        return o1[0] - o2[0];
+      }
+    });
+
+    PriorityQueue<int[]> q = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+
+    int count = 0;
+    int i = 0;
+    int now = 0;
+    while (count < jobs.length) {
+      while (i < jobs.length && jobs[i][0] <= now) {
+        q.offer(jobs[i]);
+        i++;
+      }
+
+      if (q.isEmpty()) {
+        now = jobs[i][0];
+      } else {
+        int[] temp = q.poll();
+        // 이 작업에 걸린 시간 = 현재시간 + 걸린시간 - 요청시간
+        answer += now + temp[1] - temp[0];
+        now += temp[1];
+        count++;
+      }
+    }
+
+    return answer / jobs.length;
+  }
+}
