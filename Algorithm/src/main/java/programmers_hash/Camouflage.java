b86ec86b4220d9227559d707b8864da3d184fdf6
cornchiplick <ilsunkiss@gmail.com>
2022-12-23 23:58:30 +0900

    20221223
    programmers: 전화번호 목록, 위장

---
+++ Algorithm/src/main/java/programmers_hash/Camouflage.java
@@ -0,0 +1,28 @@
+package programmers_hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Camouflage {
+  public static void main(String[] args) {
+    String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+    int result = 5;
+    System.out.println(solution(clothes));
+
+  }
+
+  public static int solution(String[][] clothes) {
+    int answer = 1;
+
+    HashMap<String, Integer> map = new HashMap<>();
+    for (int i = 0; i < clothes.length; i++) {
+      map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+    }
+
+    for (Map.Entry<String, Integer> entry : map.entrySet()) {
+      answer *= entry.getValue() + 1;
+    }
+
+    return answer-1;
+  }
+}
