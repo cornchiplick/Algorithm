1eb7c8ee234326ddf91758ef33bc881b44ac5551
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2022-12-23 18:42:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.72 ms, Memory: 95.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+public String solution(String[] participant, String[] completion) {
+    HashMap<String, Integer> map = new HashMap<>();
+    for (String player : participant) {
+      map.put(player, map.getOrDefault(player, 0) + 1);
+    }
+    for (String player : completion) {
+      map.put(player, map.get(player) - 1);
+    }
+
+    for (String player : participant) {
+      if (map.get(player).equals(1)) {
+        return player;
+      }
+    }
+
+    return null;
+  }
+}
