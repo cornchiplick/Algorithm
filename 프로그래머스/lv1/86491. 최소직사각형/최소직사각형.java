e306cdea6ed148b6304c90c9b5cfedd700c7c323
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2022-12-31 14:18:29 +0900

    [level 1] Title: 최소직사각형, Time: 3.20 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+public int solution(int[][] sizes) {
+    int smallMax = 0;
+    int bigMax = 0;
+    
+    for (int i = 0; i < sizes.length; i++) {
+      // sizes의 원소 각각을 오름차순으로 정렬
+      Arrays.sort(sizes[i]);
+      
+      // (가로 세로 중 작은 것)들 중 가장 큰 값 찾기
+      if (sizes[i][0] >= smallMax) {
+        smallMax = sizes[i][0];
+      }
+      // (가로 세로 중 큰 것)들 중 가장 큰 값 찾기
+      if (sizes[i][1] >= bigMax) {
+        bigMax = sizes[i][1];
+      }
+    }
+
+    return smallMax * bigMax;
+  }
+}
