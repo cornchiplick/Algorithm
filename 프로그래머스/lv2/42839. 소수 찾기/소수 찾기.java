dce30dbceec495926a0195cc2c2e5f1275b636cd
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2023-01-03 15:24:53 +0900

    [level 2] Title: 소수 찾기, Time: 1.31 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,70 @@
+import java.util.*;
+class Solution {
+    public HashSet<Integer> hashSet = new HashSet<>();
+  public int solution(String numbers) {
+    int count = 0;
+    // 주어진 숫자 하나씩 분리
+    Integer[] nums = spliceNumbers(numbers);
+
+    // hashSet에 가능한 모든 수를 저장
+    findNumberArr(0, nums);
+
+    // hashSet에 저장된 수가 소수인지 확인
+    Iterator<Integer> it = hashSet.iterator();
+    while (it.hasNext()) {
+      int number = it.next();
+
+      if (isPrime(number)) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+    
+      private Integer[] spliceNumbers(String numbers) {
+    String[] str = numbers.split("");
+    Integer[] nums = new Integer[str.length];
+    for (int i = 0; i < str.length; i++) {
+      nums[i] = Integer.parseInt(str[i]);
+    }
+
+    return nums;
+  }
+    
+    private void findNumberArr(Integer comb, Integer[] others) {
+    if (comb != 0) {
+      hashSet.add(comb);
+    }
+
+    for (int i = 0; i < others.length; i++) {
+      if (comb != 0) {
+        hashSet.add(comb);
+      }
+
+      int temp = others[i];
+      if (temp != -1) {
+        others[i] = -1;
+        findNumberArr(comb * 10 + temp, others);
+        others[i] = temp;
+      }
+    }
+
+  }
+    
+      private boolean isPrime(int number) {
+    if (number == 0 || number == 1) {
+      return false;
+    }
+
+    int limit = (int) Math.sqrt(number);
+    for (int i = 2; i <= limit; i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+    
+}
