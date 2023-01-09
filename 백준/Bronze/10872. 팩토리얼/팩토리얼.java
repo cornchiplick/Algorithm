4d84d5defcba620fa5963af25fcb692e96decf1f
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2023-01-09 17:08:39 +0900

    [Bronze V] Title: 팩토리얼, Time: 208 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * 백준 - 10872
+ * 브론즈V - 팩토리얼
+ * 단계별로 풀어보기 - 재귀
+ * 재귀함수 설계능력이 부족하다고 판단하여 진행하는 단계별로 풀어보기 - 재귀 편 : 반드시 재귀함수를 사용하여 풀 것이다.
+ * 전략
+ * 1) 주어진 n이 1보다 작다면 1을 return 한다. (1! == 1 이므로)
+ * 2) n이 1이 되는 시점이 재귀함수의 break시점이므로 n부터 점차 감소하여 1이 될때까지 재귀호출 하도록 설계하였다. 
+ */
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+
+    System.out.println(factorial(n));
+  }
+
+  private static int factorial(int n) {
+    if (n <= 1) {
+      return 1;
+    }
+
+    return n * factorial(n - 1);
+  }
+}
