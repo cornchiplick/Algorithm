5f51597ab08aaccc8c8c974dabbdf30083fcc021
cornchiplick <ilsunkiss@gmail.com>
2023-01-10 17:36:14 +0900

    20230110
    baekjoon: 알고리즘 수업 - 병합 정렬 1, 별 찍기 - 10, 하노이 탑 이동 순서

--- Algorithm/src/main/java/baekjoon/recurrence/MergeSort1_24060.java
+++ Algorithm/src/main/java/baekjoon/recurrence/MergeSort1_24060.java
@@ -11,11 +11,15 @@ import java.util.StringTokenizer;
  * 단계별로 풀어보기 - 재귀
  * 재귀함수 설계능력이 부족하다고 판단하여 진행하는 단계별로 풀어보기 - 재귀 편 : 반드시 재귀함수를 사용하여 풀 것이다.
  * 전략
- * 1)
+ * 1) MergeSort를 구현한다.
+ * 2) MergeSort중 sorted배열에 값을 저장할 때마다 k번째 저장인지 체크한 후, 저장된 값을 전역변수 saved에 저장한다.
+ * 3) k번째 저장이 일어나지 않았다면 -1을 출력한다.
  */
 public class MergeSort1_24060 {
   private static int[] sorted;
   private static int count = 0;
+  private static int k;
+  private static int saved = -1;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,7 +27,7 @@ public class MergeSort1_24060 {
     int n = Integer.parseInt(st.nextToken());
     int[] arr = new int[n];
     sorted = new int[n];
-    int k = Integer.parseInt(st.nextToken());
+    k = Integer.parseInt(st.nextToken());
 
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < arr.length; i++) {
@@ -32,13 +36,9 @@ public class MergeSort1_24060 {
 
     br.close();
 
-    // 정렬?
+    mergeSort(arr, 0, arr.length - 1);
 
-    if (count <= k) {
-      System.out.println(count);
-    } else {
-      System.out.println(-1);
-    }
+    System.out.println(saved);
 
   }
 
@@ -50,19 +50,35 @@ public class MergeSort1_24060 {
     // 작은 데이터부터 삽입
     while (p <= middle && q <= end) {
       if (arr[p] <= arr[q]) {
+        count++;
+        if (count == k) {
+          saved = arr[p];
+        }
         sorted[r++] = arr[p++];
       } else {
+        count++;
+        if (count == k) {
+          saved = arr[q];
+        }
         sorted[r++] = arr[q++];
       }
     }
 
     // 왼쪽이 남았을 경우
     while (p <= middle) {
+      count++;
+      if (count == k) {
+        saved = arr[p];
+      }
       sorted[r++] = arr[p++];
     }
 
     // 오른쪽이 남았을 경우
     while (q <= end) {
+      count++;
+      if (count == k) {
+        saved = arr[q];
+      }
       sorted[r++] = arr[q++];
     }
 
---
+++ Algorithm/src/main/java/baekjoon/recurrence/TowerOfHanoi_11729.java
@@ -0,0 +1,56 @@
+package baekjoon.recurrence;
+
+import java.util.Scanner;
+
+/**
+ * 백준 - 11729
+ * 실버I - 하노이 탑 이동 순
+ * 단계별로 풀어보기 - 재귀
+ * 재귀함수 설계능력이 부족하다고 판단하여 진행하는 단계별로 풀어보기 - 재귀 편 : 반드시 재귀함수를 사용하여 풀 것이다.
+ * 전략
+ * 1) n개의 원판을 1에서 3번 장대로 옮기는 것은 n개의 원판을 1에서 2번 장대로 옮기는 것과 같다.
+ * 2) n개의 원판을 1에서 2번 장대로 옮기는 것은 n-1개의 원판을 1에서 3번 장대로 옮긴 후,
+ *    가장 아래의 원판 하나를 2번 장대로 옮기고 다시 n-1개의 원판을 3에서 1번 장대로 옮기는 것과 같다.
+ * 3) 하나의 원판만 옮기면 될 때까지 재귀호출 한다.
+ */
+public class TowerOfHanoi_11729 {
+
+  private static StringBuilder sb = new StringBuilder();
+
+  // 원판을 옮길 때마다 횟수를 카운트한다.
+  private static int count = 0;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+
+    hanoi(1, 3, n);
+
+    System.out.printf("%d\n%s", count, sb);
+
+  }
+
+  private static void hanoi(int x, int y, int n) {
+    // 1 2 3번 장대의 번호 합은 6이 되므로 두 개의 장대 번호가 주어진다면, 나머지 하나의 장대번호를 알 수 있다.
+    int z = 6 - x - y;
+
+    // 원판 하나만 옮길때(종결조건)
+    if (n == 1) {
+      sb.append(x).append(' ').append(y).append('\n');
+      count++;
+      return;
+    }
+
+    // n-1개의 원판을 목표장대가 아닌 다른 장대로 옮긴 후
+    hanoi(x, z, n - 1);
+
+    // 맨 아래의 원판을 목표 장대로 옮기고
+    sb.append(x).append(' ').append(y).append('\n');
+    count++;
+
+    // 목표장대가 아닌 곳에 있는 n-1개의 원판을 다시 목표장대로 옮긴다.
+    hanoi(z, y, n - 1);
+
+  }
+}
