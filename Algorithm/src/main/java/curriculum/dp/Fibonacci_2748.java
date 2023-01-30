6af95b5b764b51af0fc5d3e695c2126ca1c6381b
cornchiplick <ilsunkiss@gmail.com>
2023-01-30 15:19:54 +0900

    20230129
    baekjoon: 피보나치 수 2

---
+++ Algorithm/src/main/java/curriculum/dp/Fibonacci_2748.java
@@ -0,0 +1,31 @@
+package curriculum.dp;
+
+import java.util.Scanner;
+
+/**
+ * 백준 - 2748
+ * 브론즈I - 피보나치 수 2
+ * 전략
+ * 1) 피보나치 수열 구현
+ * 2) dynamic programming 구현
+ * 주의사항
+ * 1) n의 값이 너무 커지면 int자료형으로는 표현할 수 없다.
+ */
+public class Fibonacci_2748 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+
+    long[] dp = new long[91];
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    System.out.println(dp[n]);
+
+  }
+}
