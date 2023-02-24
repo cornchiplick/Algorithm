fe25b1965521abae16b1463368edd9a070aed9e3
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2023-02-24 16:17:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * 백준 - 10430
+ * 브론즈V - 나머지
+ * 전략
+ * 1) 기초적인 사칙연산 문제
+ * 2) 숫자 범위 주의
+ */
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
+    sc.close();
+  }
+}
