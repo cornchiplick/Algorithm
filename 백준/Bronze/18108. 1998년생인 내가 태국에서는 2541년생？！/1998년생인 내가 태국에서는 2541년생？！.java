7cd193fdca2ceb24db502f69830523623906fc47
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2023-02-24 16:00:45 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * 백준 - 18108
+ * 브론즈V - 1998년생인 내가 태국에서는 2541년생?!
+ * 전략
+ * 1) 기초적인 입출력 문제
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println((new Scanner(System.in).nextInt())-543);
+  }
+}
