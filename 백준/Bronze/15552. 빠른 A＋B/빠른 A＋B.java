10a8c665d96159bc1c3a109274960a6d605d5957
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2023-02-24 17:10:52 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 864 ms, Memory: 236316 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 - 15552
+ * 브론즈IV - 빠른 A+B
+ * 전략
+ * 1) 기초적인 반복문 문제
+ */
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int t = Integer.parseInt(br.readLine());
+    StringTokenizer st;
+
+    for (int i = 0; i < t; i++) {
+      st = new StringTokenizer(br.readLine());
+      bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+    }
+
+    br.close();
+
+    bw.close();
+
+  }
+}
