9fcaf9caca5aa8c5d280b9c50681a133dd4111f2
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2023-02-24 15:56:50 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * 백준 - 10926
+ * 브론즈V - ??!
+ * 전략
+ * 1) 기초적인 입출력 문제
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println((new Scanner(System.in).nextLine()) + "??!");
+  }
+}
