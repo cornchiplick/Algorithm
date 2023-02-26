36ea929e433f4b9528c9d3f7023f09aa7261a831
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2023-02-26 14:21:35 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 - 10807
+ * 브론즈V - 개수 세기
+ * 전략
+ * 1) 기초적인 문제
+ */
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int[] arr = new int[Integer.parseInt(br.readLine())];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int num = Integer.parseInt(br.readLine());
+    int temp;
+    int count = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      temp = Integer.parseInt(st.nextToken());
+      if (temp == num) {
+        count++;
+      }
+    }
+
+    System.out.println(count);
+
+  }
+}
