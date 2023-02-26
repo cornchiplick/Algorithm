9dc730126f85217e3d0ffc9d3ec7821df20a9fc0
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2023-02-26 14:51:03 +0900

    [Bronze II] Title: 공 바꾸기, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 - 10813
+ * 브론즈II - 공 바꾸기
+ * 전략
+ * 1) 기초적인 문제
+ */
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = i + 1;
+    }
+
+    int a;
+    int b;
+    int temp;
+
+    for (int i = 0; i < m; i++) {
+      st = new StringTokenizer(br.readLine());
+      a = Integer.parseInt(st.nextToken()) - 1;
+      b = Integer.parseInt(st.nextToken()) - 1;
+
+      temp = arr[a];
+      arr[a] = arr[b];
+      arr[b] = temp;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int element : arr) {
+      sb.append(element).append(' ');
+    }
+
+    System.out.println(sb);
+  }
+}
