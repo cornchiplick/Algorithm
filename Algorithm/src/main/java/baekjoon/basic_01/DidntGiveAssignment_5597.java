f22c0deef40c8fe75233c7ce81d1df98271ee6bf
cornchiplick <ilsunkiss@gmail.com>
2023-02-26 14:59:18 +0900

    20230226
    baekjoon:
    개수 세기,
    공 넣기,
    공 바꾸기,
    과제 안 내신 분..?

---
+++ Algorithm/src/main/java/baekjoon/basic_01/DidntGiveAssignment_5597.java
@@ -0,0 +1,28 @@
+package baekjoon.basic_01;
+
+import java.util.Scanner;
+
+/**
+ * 백준 - 5597
+ * 브론즈V - 과제 안 내신 분..?
+ * 전략
+ * 1) 기초적인 문제
+ */
+public class DidntGiveAssignment_5597 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    boolean[] arr = new boolean[30];
+
+    for (int i = 0; i < 28; i++) {
+      arr[sc.nextInt() - 1] = true;
+    }
+
+    int count = 0;
+    for (boolean element : arr) {
+      count++;
+      if (!element) {
+        System.out.println(count);
+      }
+    }
+  }
+}
