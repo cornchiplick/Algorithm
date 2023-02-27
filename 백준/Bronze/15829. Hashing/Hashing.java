cd42ad06acd5757fa091f69c311f3ab2f9df4135
Kim Mingu <59365874+cornchiplick@users.noreply.github.com>
2023-02-27 13:40:48 +0900

    [Bronze II] Title: Hashing, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,19 +1,38 @@
 import java.util.Scanner;
 
+/**
+ * 백준 - 15829
+ * 브론즈II - Hashing
+ * 전략
+ * 1) long 자료형의 범위를 넘어가지 않도록 중간과정에서 계속 %연산을 통해 값을 줄여준다.
+ */
 public class Main {
+  private static final long factor = 1234567891;
+
   public static void main(String[] args) {
-
-    Scanner in = new Scanner(System.in);
-
-    int len = in.nextInt();
-    String str = in.next();
-    int h = 0;
+    Scanner sc = new Scanner(System.in);
+    int len = sc.nextInt();
+    String str = sc.next();
+    long c;
+    long temp;
+    long sum;
+    long answer = 0;
 
 
     for (int i = 0; i < len; i++) {
-      h += (str.charAt(i) - 'a' + 1) * ((int) Math.pow(31, i));
+      c = str.charAt(i) - 'a' + 1;
+
+      // 31의 i제곱의 나머지
+      temp = 1;
+      for (int j = 0; j < i; j++) {
+        temp *= 31;
+        temp %= factor;
+      }
+
+      sum = (c * temp) % factor;
+      answer = (answer + sum) % factor;
     }
 
-    System.out.println(h%1234567891);
+    System.out.println(answer);
   }
 }
